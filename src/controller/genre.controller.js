ecbaacb56f2d5a1ed20bbe2cdcce1cea7a1fb3ab
AlikhanSmagulov <alikhan.smagulov@student.nhlstenden.com>
2024-01-15 20:46:27 +0100

    Added functions to genre controller and routes

--- src/controller/genre.controller.js
+++ src/controller/genre.controller.js
@@ -1,5 +1,21 @@
-const Genre = {
+const Genres = {
     Documentary: "Documentary",
     Horror: "Horror",
     Sci_fi: "Sci_fi"
 }
+
+const genreController = {
+    getAllGenres: (request, response) => {
+        response.json(Object.values(Genres))
+    },
+
+    getGenreById: (request, response) => {
+        const genreId = request.params.body;
+
+        if(Genres[genreId]) {
+            response.json(Genres[genreId]);
+        } else {
+            response.status(404).json({error: 'Genre is not found'});
+        }
+    }
+};
--- src/routes/genre.routes.js
+++ src/routes/genre.routes.js
@@ -6,6 +6,7 @@ const GenreController = require('../controller/genre.controller');
 
 const genreController = new GenreController();
 
-
+application.get('/genres', genreController.getAllGenres);
+application.get('/genres', genreController.getGenreById);
 
 module.exports = router;
