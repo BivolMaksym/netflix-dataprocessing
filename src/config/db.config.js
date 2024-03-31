7e7b432f95204a8fb76da44ef1e010aa4f800506
AlikhanSmagulov <alikhan.smagulov@student.nhlstenden.com>
2024-03-31 21:45:45 +0200

    Added password for config file

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -6,7 +6,7 @@ class DB {
             host: '127.0.0.1',
             port: 3306,
             user: 'root',
-            password: '',
+            password: 'qwerty',
             database: 'netflix',
         });
 
